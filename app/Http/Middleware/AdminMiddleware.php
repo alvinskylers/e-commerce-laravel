62aa6675d8b298f3cddf57ec491b14b674667e53
alvinskylers <alvinskylers.dev@gmail.com>
2026-03-01 14:57:21 +0800

    add: admin middleware implemented authentication and authorization with admin middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->user_type == 'admin') {
+            return $next($request);
+        }
+
+        if(Auth::check() && Auth::user()->user_type == 'user'){
+            abort(403, 'Unauthorized Access.');
+        }
+
+        abort(403, 'Unauthorized Access.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+
 Route::get('/', function () {
     return view('index');
 })->name('index');
@@ -13,6 +15,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('admin')->group(function () {
+   Route::get('/admin_only', [AdminController::class,'admin_only'])->name('admin.only');
 });
 
 require __DIR__.'/auth.php';
