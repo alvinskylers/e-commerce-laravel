f8d94e80502d0746cc95ecad08978c7b4818ce5b
alvinskylers <alvinskylers.dev@gmail.com>
2026-03-01 14:12:31 +0800

    add: e-commerce homepage reroute register and login buttons to the register and login page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', [UserController::class,'index'] )->middleware(['auth', 'verified'])->name('dashboard');
